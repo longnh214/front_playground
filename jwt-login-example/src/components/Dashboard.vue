727118dd93e705345a4d941a09f93290eca384e4
Nakhoon <cnh6123@naver.com>
2024-12-24 13:54:56 +0900

    [jwt-login-example][UPDATE] test controller 호출 적용

--- jwt-login-example/src/components/Dashboard.vue
+++ jwt-login-example/src/components/Dashboard.vue
@@ -1,15 +1,21 @@
 <template>
     <div>
       <h1>Dashboard</h1>
-      <p>Welcome, {{ user }}</p>
+      <p>{{ message }}, {{ user }}</p>
       <button @click="handleLogout">Logout</button>
     </div>
   </template>
   
   <script>
   import { mapGetters, mapActions } from 'vuex';
+  import apiClient from '@/utils/axios';
 
   export default {
+    data(){
+      return{
+        message: '',
+      };
+    },
     computed: {
       ...mapGetters('auth', ['getUsername']),
       user() {
@@ -22,6 +28,19 @@
         this.logout();
         this.$router.push('/login');
       },
+    },
+    mounted() {
+    // 서버에 요청을 보내 데이터를 가져옴
+      apiClient
+        .get('/test') // API 호출
+        .then((response) => {
+          console.log(response.data);
+          this.message = response.data; // 응답 데이터를 message에 저장
+        })
+        .catch((error) => {
+          console.error('Error fetching message:', error); // 에러 처리
+          this.message = 'Error fetching data'; // 오류 메시지 표시
+        });
     },
   };
   </script>
